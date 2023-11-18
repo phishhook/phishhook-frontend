285eec86f9b1583c5143ac6a82c0ff69bbc9eed0
Lucas Smith <smithlskotn@gmail.com>
2023-11-18 01:15:34 -0600

    connected app to database and ml module

--- PhishhookFrontend/PhishhookFrontend/app/src/main/java/com/example/networktest/MainActivity.java
+++ PhishhookFrontend/PhishhookFrontend/app/src/main/java/com/example/networktest/MainActivity.java
@@ -5,30 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONObject;
-
-import java.net.URI;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String MLApiURL = " http://ec2-34-226-215-44.compute-1.amazonaws.com/?url=";
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.queue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
-        String ApiURL = "https://www.google.com";
-        StringRequest jsonObjectRequest = new StringRequest (Request.Method.GET, ApiURL ,dataListener,errorListener);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(jsonObjectRequest);
+        String LinkApiURL = "http://ec2-18-224-251-242.us-east-2.compute.amazonaws.com:8080/links";
+        StringRequest jsonLinkRequest = new StringRequest (Request.Method.GET, LinkApiURL , linkDataListener, errorListener){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                // TODO: store key in env file
+                params.put("X-API-KEY", "phishhookRyJHCenIz97Q5LIDPmHhDyg9eddxaBO29omDuzM1D5BsDRKH5mo3j8pmBehoO2Roj0Z4zWuDHlNW4AJVrSnLZF6lUravmyje13YB1LBriXHxYlxLUDYeXmV");
+                return params;
+            }
+        };
+
+
+        queue.add(jsonLinkRequest);
         System.out.println(getIntent().getData() + "+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*++**+");
 
         Uri my_uri= getIntent().getData();
@@ -41,8 +58,12 @@ public class MainActivity extends AppCompatActivity {
         i.setPackage("com.android.chrome");
         i.setAction(Intent.ACTION_VIEW);
         System.out.println(getIntent().getData() + "  ***********");
+        linkAnalysis(getIntent().getData().toString());
         //Log.d("LINK", getIntent().getData().toString());
         i.setData(getIntent().getData());
+
+
+
         startActivity(i);
 
     }
@@ -50,15 +71,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        System.out.println("TEST #######");
+        //System.out.println("TEST #######");
     }
 
-    Response.Listener<String> dataListener  = new Response.Listener<String>() {
+    protected void linkAnalysis(String url) {
+        StringRequest jsonMLRequest = new StringRequest (Request.Method.POST, MLApiURL + url, MLDataListener, errorListener){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                // TODO: store key in env file
+                params.put("X-API-KEY", "phishhookRyJHCenIz97Q5LIDPmHhDyg9eddxaBO29omDuzM1D5BsDRKH5mo3j8pmBehoO2Roj0Z4zWuDHlNW4AJVrSnLZF6lUravmyje13YB1LBriXHxYlxLUDYeXmV");
+                return params;
+
+            }
+        };
+        // ML model can take around 10 seconds to process a url; timeout set to 15 seconds just to be safe
+        jsonMLRequest.setRetryPolicy(new DefaultRetryPolicy(
+                15000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(jsonMLRequest);
+    }
+
+    Response.Listener<String> linkDataListener = new Response.Listener<String>() {
 
         @Override
         public void onResponse(String response) {
 
-            //System.out.println(response.toString());
+            System.out.println(response.toString());
+        }
+    };
+
+    Response.Listener<String> MLDataListener = new Response.Listener<String>() {
+
+        @Override
+        public void onResponse(String response) {
+
+            System.out.println(response.toString() + "&&&&&&&&&&&&&&&");
         }
     };
 
@@ -66,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onErrorResponse(VolleyError error) {
-            System.out.println(error);
+            System.out.println(error + "#########");
         }
     };
 }
